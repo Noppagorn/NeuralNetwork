35596249e45c531910b68d7faf90f18580c31ea8
NoppaGorn <tongnop2540@gmail.com>
2020-09-21 01:22:14 +0700

    adding handle layer in Class Net __init__

--- Net.py
+++ Net.py
@@ -2,6 +2,11 @@ class Net:
     def __init__(self,topology,layers):
         self.topology = topology
         self.layers = layers
+        self.layerNums = len(topology)
+        for layerNum in range(0,self.layerNums):
+            layers.append(None)
+            for nuronNum in range(0,len(topology[layerNum])):
+                pass
     def feedForward(self,inputVals):
         pass
     def backProp(self,targetVals):
