08c0b0a8334559301c17482e1d062d02e5031b43
tong <tongnop2540@gmail.com>
2020-10-16 13:59:37 +0700

    edit main for test pull request conflict

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from Net import Net
-timeVideo="45:15"
+#timeVideo="4"
+timeVideo="4.554"
+a=10
 if __name__ == '__main__':
     topology = []
     topology.append(3)
     topology.append(2)
     topology.append(1)
-    print(topology)
+    #print(topology)
     myNet = Net(topology)
     inputVals = [1,2,3,4]
     myNet.feedForward(inputVals)
